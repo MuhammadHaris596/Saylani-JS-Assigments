9ab40ba7d034452b9b45a537a9ad31c51d8fce03
Muhammad Haris <134528490+MuhammadHaris596@users.noreply.github.com>
2024-09-14 16:05:58 +0500

    Add files via upload

---
+++ chp1.js
@@ -0,0 +1,19 @@
+
+//Qno 1
+window.alert("Hello, How are you :)" );
+
+//Qno 2
+window.alert("Error! Please enter a valid password.")
+
+//Qno 3
+alert("Welcome to JS Land" + "\n" + "Happy Coding!");
+
+
+
+//Qno 6
+alert("Welcome to JS Land");
+
+alert("Prevent this page from creating additional dialogs");
+
+
+console.log("Hello.... I can run JS through my web browsers's console");
